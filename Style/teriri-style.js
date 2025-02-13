d3c1d0a8a6a71e120e035b3d3804a785990a267d
紺野ゆうづき <konno_yuuzuki@outlook.com>
2025-02-13 22:54:07 +0800

    更新

--- Style/teriri-style.js
+++ Style/teriri-style.js
@@ -1,11 +1,24 @@
-const el = document.querySelector('.shadow-root-host')
-const shadowRoot = el.shadowRoot
+window.onload = () => {
+    const el = document.querySelector('.shadow-root-host')
+    const shadowRoot = el.shadowRoot
 
-const style = document.createElement('style')
-style.textContent = `
-    .monaco-scrollable-element {
+    const style = document.createElement('style')
+    style.textContent = `
+    .context-view.monaco-menu-container::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: block;
         background-color: transparent !important;
         backdrop-filter: blur(20px) brightness(120%);
+        z-index: -1;
     }
-`
-shadowRoot.appendChild(style)
+    .monaco-scrollable-element {
+        background-color: transparent !important;
+    }
+    `
+    shadowRoot.appendChild(style)
+}
