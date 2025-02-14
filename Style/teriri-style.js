45ed8ab04cc9eda9ffa06d08e26c96d18b53dcca
紺野ゆうづき <konno_yuuzuki@outlook.com>
2025-02-14 18:40:46 +0800

    更新

--- Style/teriri-style.js
+++ Style/teriri-style.js
@@ -1,33 +1,38 @@
-window.onload = () => {
-    const editorContainer = document.querySelector('.editor-container')
-    console.log('editorContainer', editorContainer)
-    const contextMenuListener = editorContainer.addEventListener(
-        'contextmenu',
-        function (event) {
-            console.log('contextMenuListener')
-            const el = document.querySelector('.shadow-root-host')
-            const shadowRoot = el.shadowRoot
+;(function () {
+    'use strict'
 
-            const style = document.createElement('style')
-            style.textContent = `
-    .context-view.monaco-menu-container::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        display: block;
-        background-color: transparent !important;
-        backdrop-filter: blur(20px) brightness(120%);
-        z-index: -1;
+    function waitForElement(el, fn) {
+        const timer = setInterval(() => {
+            const elRef = document.querySelector(el)
+            if (elRef) {
+                fn(elRef)
+                clearInterval(timer)
+            }
+        }, 10)
     }
-    .monaco-scrollable-element {
-        background-color: transparent !important;
+
+    function addCustomContextMenuStyle(shadowRootEl) {
+        const shadowRoot = shadowRootEl.shadowRoot
+
+        const style = document.createElement('style')
+        style.textContent = `
+        .context-view.monaco-menu-container::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            display: block;
+            background-color: transparent !important;
+            backdrop-filter: blur(20px) brightness(120%);
+            z-index: -1;
+        }
+        .monaco-scrollable-element {
+            background-color: transparent !important;
+        }
+        `
+        shadowRoot.appendChild(style)
     }
-    `
-            shadowRoot.appendChild(style)
-            removeEventListener('contextmenu', contextMenuListener)
-        },
-    )
-}
+    waitForElement('.shadow-root-host', addCustomContextMenuStyle)
+})()
